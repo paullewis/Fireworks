b3ec3a230d540e75851fe6ca5b8d277cd392febd
jmedley@google.com <jmedley@google.com>
2016-03-31 16:15:06 -0700

    Add welcome back message.

--- js/fireworks.js
+++ js/fireworks.js
@@ -438,4 +438,10 @@ var FireworkExplosions = {
 // Go
 window.onload = function() {
   Fireworks.initialize();
+  var howLaunched = window.location.search.substring(1).split('=')[0];
+  if (howLaunched === 'homescreen') {
+    var welcome = document.getElementById('welcome-back');
+    welcome.style.display = 'block';
+    welcome.classList.remove('isPaused');
+  }
 };
